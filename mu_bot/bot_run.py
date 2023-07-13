71f455a0e76c65f3506f9251a96c9a8ccb68f713
vladke <130487670+Vladke2@users.noreply.github.com>
2023-07-13 21:35:19 +0300

    Update bot_run.py

--- mu_bot/bot_run.py
+++ mu_bot/bot_run.py
@@ -1,9 +1,7 @@
 from aiogram.utils import executor
-from mu_bot.botss import dp, bot
-from mu_bot.commands import com_hellp
-from mu_bot.commands import com_start
-print(bot)
-print(com_hellp)
-print(com_start)
+from botss import dp
+import commands
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
