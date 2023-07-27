8cb814e6fd7470fd18230b08652dca605317b293
vladke <130487670+Vladke2@users.noreply.github.com>
2023-07-27 21:19:05 +0300

    Update com_hellp.py

--- mu_bot/commands/com_hellp.py
+++ mu_bot/commands/com_hellp.py
@@ -1,18 +1,30 @@
 from aiogram import types
-from mu_bot.botss import dp, bot
+from botss import dp, bot
 
 
-@dp.message_handler(commands=['hellp'])
+@dp.message_handler(commands=['hellp','help'])
 async def hellp_info(message):
+    gif = 'bv0j-help'
+    await bot.send_animation(gif)
+    url = 'https://github.com/Vladke2'
+    bot_data = await bot.get_me()
     button = types.InlineKeyboardMarkup(
         inline_keyboard=[
         [types.InlineKeyboardButton('Розпочати', callback_data='starts')],
+            [types.InlineKeyboardButton('список команд', callback_data='list_of_commands')]
     ])
-    await message.reply("Hellow!\nI'm guess_the_number_Bot!\nPowered by aiogram.\nCreated by Vlad\n/start",
+    await message.reply(f"Hellow!\nI'm {bot_data.first_name}!\nPowered by aiogram.\
+    \nCreated by Vlad({url})",
                         reply_markup=button)
 
 
 @dp.callback_query_handler(lambda callb: callb.data == "starts")
 async def execute_random(callb: types.CallbackQuery):
     await callb.answer('/start')
-    await callb.message.reply('Натискай сюди щоб розпочати\n/start')
+    await callb.message.answer('(🇺🇦)Натискай сюди щоб розпочати\n/start \
+                              \n(🇬🇧)Click here to start\n/start')
+
+@dp.callback_query_handler(lambda callb: callb.data == "list_of_commands")
+async def execute_random(callb: types.CallbackQuery):
+    await callb.answer('/start,/clear,/cancel,/hellp,/help')
+    await callb.message.reply('/start\n/clear\n/cancel\n/hellp,/help')
